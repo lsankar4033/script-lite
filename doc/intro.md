cce99c9d0c73ffd510cb61560e9787338eb5ce6e
lsankar4033 <lsankar4033@gmail.com>
2021-05-21 08:31:37 -0700

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to script-lite
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/script_lite/core_test.clj
@@ -0,0 +1,7 @@
+(ns script-lite.core-test
+  (:require [clojure.test :refer :all]
+            [script-lite.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
