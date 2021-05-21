dd61421105717924ea0a83a0c32edd9cabcc8e70
lsankar4033 <lsankar4033@gmail.com>
2021-05-21 14:51:07 -0700

    High level exec loop

--- src/script_lite/core.clj
+++ src/script_lite/core.clj
@@ -1,6 +1,26 @@
 (ns script-lite.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; op structure = {:op :input}
+;; maybe i start by just writing a program that processes OP_ADD, OP_EQUAL, OP_VERIFY?
+;; what is scriptSig? Maybe don't need to worry about that for now
+
+(defn exec
+  "Run the specified ops. Returns true or false"
+  [ops]
+  (let [{:keys [failed stack]} (reduce exec-op
+                                       {:failed false, :stack []}
+                                       ops)]
+    (if failed
+      false
+
+      ; if script completes, is considered valid iff top of stack exists and == true
+      (true? (first stack)))))
+
+(defn exec-op
+  "Run op with the given stack and return map of form {:failed, :stack}"
+  [op {:keys [failed stack]}]
+  (if failed
+    {:failed failed, :stack stack}
+
+    ; TODO: run op
+    ))
