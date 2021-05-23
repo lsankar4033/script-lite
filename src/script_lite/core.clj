efade3e3342d34e1f5c16bae621a9f2242830f1a
lsankar4033 <lsankar4033@gmail.com>
2021-05-23 12:43:54 -0700

    Fix bug

--- src/script_lite/core.clj
+++ src/script_lite/core.clj
@@ -1,22 +1,20 @@
 (ns script-lite.core
   (:require [script-lite.op :as op]))
 
-;; TODO: when I add crypto, sig+pubkey are stuck onto the stack *first*!
-
 (defn exec-ops
   [ops]
-  (reduce op/exec-op
-          {:failed false, :stack []}
-          (fn [acc op]
+  (reduce (fn [acc op]
             (if (:failed acc)
               (reduced acc)
-              (op/exec-op op (:stack acc))))))
+              (op/exec-op op (:stack acc))))
+
+          {:failed false, :stack []}
+
+          ops))
 
 (defn exec
   "Run the specified ops. Returns true or false. Doesn't currently support non-op bytes in the script."
   [ops]
-  ; TODO: should work through a list of bytes b/c some bytestrings will correspond to other data that needs to
-  ; be put on the stack
   (let [{:keys [failed stack]} exec-ops]
 
     ; This triple nesting is kinda gross...
